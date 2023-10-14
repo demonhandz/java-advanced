3dc931a856affc07d2b32f4b225b1f446675a70d
Georgi Danailov <70630746+demonhandz@users.noreply.github.com>
2023-10-14 22:03:18 +0300

    Update

--- src/multidimentional_arrays/P6StringMatrixRotation.java
+++ src/multidimentional_arrays/P6StringMatrixRotation.java
@@ -8,17 +8,12 @@ public class P6StringMatrixRotation {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // TODO extract angle from the input string
-        int rotation = Integer.parseInt(scanner.nextLine()
-                .split("\\(")[1]
-                .replaceAll("\\)", ""));
-
+        int rotation = Integer.parseInt(scanner.nextLine().split("\\(")[1].replaceAll("\\)", ""));
         int rotationAngle = rotation % 360;
 
         String input = scanner.nextLine();
-
         List<String> words = new ArrayList<>();
-        int maxLength = Integer.MIN_VALUE;
+        int maxLength = input.length(); // Initialize maxLength with the length of the first line
 
         while (!"END".equals(input)) {
             words.add(input);
@@ -30,25 +25,17 @@ public class P6StringMatrixRotation {
 
         int rows = words.size();
         int cols = maxLength;
-
         char[][] matrix = new char[rows][cols];
 
         fillMatrix(matrix, words);
 
-
-        switch (rotationAngle) {
-            case 90:
-                matrix = rotate90(matrix, rows, cols);
-                break;
-            case 180:
-                matrix = rotate90(matrix, rows, cols);
-                matrix = rotate90(matrix, rows, cols);
-                break;
-            case 270:
-                matrix = rotate90(matrix, rows, cols);
-                matrix = rotate90(matrix, rows, cols);
-                matrix = rotate90(matrix, rows, cols);
-                break;
+        // Handle arbitrary rotation angles
+        int rotations = (rotationAngle / 90) % 4;
+        for (int i = 0; i < rotations; i++) {
+            matrix = rotate90(matrix, rows, cols);
+            int temp = rows;
+            rows = cols;
+            cols = temp;
         }
 
         printMatrix(matrix);
@@ -57,9 +44,8 @@ public class P6StringMatrixRotation {
     private static char[][] rotate90(char[][] matrix, int rows, int cols) {
         char[][] rotatedMatrix = new char[cols][rows];
         for (int col = 0; col < cols; col++) {
-            int colCounter = 0;
-            for (int row = rows - 1; row >= 0; row--) {
-                rotatedMatrix[col][colCounter++] = matrix[row][col];
+            for (int row = 0; row < rows; row++) {
+                rotatedMatrix[col][row] = matrix[rows - 1 - row][col];
             }
         }
         return rotatedMatrix;
