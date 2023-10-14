bb6ada28a899efc00bd7d6d914de2bf15afc035a
Georgi Danailov <70630746+demonhandz@users.noreply.github.com>
2023-10-14 21:41:51 +0300

    Update

---
+++ src/lab_defining_classes/p3_bank_account/BankAccount.java
@@ -0,0 +1,31 @@
+package lab_defining_classes.p3_bank_account;
+
+public class BankAccount {
+
+    private static double interestRate = 0.02;
+    private static int idCounter = 1;
+
+    private int id;
+    private double balance;
+
+    public BankAccount() {
+        this.id = idCounter;
+        idCounter++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getInterest(int years) {
+        return interestRate * years * balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+}
